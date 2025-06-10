528af9ae7f6cd721fc94b23d2d6478ef6f98987c
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-10 23:57:36 +0200

    Updated sum of 2 numbers

--- multiply_numbers.cpp
+++ multiply_numbers.cpp
@@ -3,6 +3,9 @@
 
 int main()
 {
-    
+    std::cout << "Enter two numbers to multiply:\n" << std::endl;   // endline flushes the buffer
+    int v0 = 0, v1 = 0;
+    std::cin >> v0 >> v1;                                           // the input operator returns itself
+    std::cout << "The product of " << v0 << " and " << v1 << " is: " << v0*v1 << std::endl;
     return 0;
 }
