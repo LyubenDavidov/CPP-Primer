1b0cb20ab260ea7db5698d50d1ea9d5e8772781e
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-11 20:57:30 +0200

    Added while loop sum of numbers

---
+++ sum_integers.cpp
@@ -0,0 +1,9 @@
+// PROGRAM TO SUM INTEGERS 1 TO 10
+#include <iostream>
+
+int main()
+{
+    int sum = 0, val = 0;
+    // keep executing the while as long as val is less than or equal to 10
+    return 0;
+}
