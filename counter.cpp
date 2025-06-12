879d1be385e3356792100509490a3c17463ce0a7
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-12 10:33:34 +0200

    Updated value counter

--- counter.cpp
+++ counter.cpp
@@ -7,10 +7,20 @@ int main()
     int currVal = 0, val = 0;
 
     // read first number and ensure we have data to process
-    if (std::cin >> currVal)
-    {
-        int cnt = 1;    // store the count for the current value we're processing
+    if (std::cin >> currVal) {
+        int cnt = 1;                // store the count for the current value we're processing
+        while (std::cin >> val) {   // read the remaining numbers
+            if (val == currVal) {   // if the values are the same
+                ++cnt;              // add 1 to count
+            }
+            else {
+                std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+                currVal = val;      // remember the new value
+                cnt = 1;            // reset counter
+            }
+        }
+        // remember to print the count for the last value in the file (if end-of-file occurs)
+        std::cout << currVal << " occurs " << cnt << " times" << std::endl;
     }
-
     return 0;
 }
