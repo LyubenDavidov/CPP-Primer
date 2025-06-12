cf1e0fa7d1436cb9594c6d368a006b842d2f0fe2
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-12 10:02:36 +0200

    Integer sum wit user input

--- sum_integers.cpp
+++ sum_integers.cpp
@@ -7,21 +7,23 @@ int main()
     // keep executing the while as long as val is less than or equal to 10
 
     // make the user sum integers in a provided range
-    int low_range, high_range;
+    int lower_range, upper_range;
     std::cout << "Enter two numbers for the lower and upper range of summing integers" << std::endl;
-    std::cin >> low_range >> high_range;
-    if (low_range >= high_range)
+    std::cin >> lower_range >> upper_range;
+    if (lower_range >= upper_range)
     {
-        std::cout << "Error: Upper range border is lower/equal than/to lower range one. Terminating..." << std::endl;
+        std::cout << "Error: Upper range must be higher than lower range integer. Terminating..." << std::endl;
         return 0;
     }
 
+    // calculate the iterations
+    int val_range = upper_range - lower_range;
 
-    while (val <= 10)
+    while (val <= val_range)
     {
-        sum += val; // assign sum+val to sum
-        ++val;      // increase val by 1
+        sum += lower_range; // assign sum+val to sum
+        ++val;              // increase val by 1
     }
-    std::cout << "Sum of 1 to 10 inclusive is: " << sum << std::endl;
+    std::cout << "Sum of integers " << lower_range << " to " << upper_range << " inclusive is: " << sum << std::endl;
     return 0;
 }
