9735400c0a38f1e7583b7b57453985d76ad6810b
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-12 10:25:50 +0200

    Value counter

---
+++ counter.cpp
@@ -0,0 +1,16 @@
+// PROGRAM TO COUNT HOW MANY TIME A VALUE OCCURS
+#include <iostream>
+
+int main()
+{
+    // currVal is the number we're counting, we read new values into val
+    int currVal = 0, val = 0;
+
+    // read first number and ensure we have data to process
+    if (std::cin >> currVal)
+    {
+        int cnt = 1;    // store the count for the current value we're processing
+    }
+
+    return 0;
+}
