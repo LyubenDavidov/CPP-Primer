dd9b600b007e072192c2d39dd0876f0e7c5ad522
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-15 19:11:25 +0200

    Played with signed unsigned

--- variables.cpp
+++ variables.cpp
@@ -8,8 +8,12 @@ int main() {
     std::cout << u1 - u0 << std::endl; //out: 32
     std::cout << u0 - u1 << std::endl; //out: 4294967264 (2^32) + (10-42) wrapped output 32 bit system
 
-    int i0 = 10, i1 = 42;
-    std::cout << i1 - i0 << std::endl; //out: 32
-    std::cout << i0 - i1 << std::endl; //out: -32
+    int i0 = 11, i1 = 42;
+    std::cout << i1 - i0 << std::endl; //out: 31
+    std::cout << i0 - i1 << std::endl; //out: -31
+
+    std::cout << u0 - i0 << std::endl; //out: 4294967295 (2^32) + (10-11)
+    std::cout << i0 - u0 << std::endl; //out: 1
+    std::cout << i0 - u1 << std::endl; //out: 4294967295 (2^32) + (11-42)
     return 0;
 }
