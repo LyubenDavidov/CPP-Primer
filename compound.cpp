c80f34c2d4858a8f5ae0ada9e7b00f74aabdb2cd
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-19 23:52:30 +0200

    Updated compound

--- compound.cpp
+++ compound.cpp
@@ -3,6 +3,19 @@
 
 
 int main() {
+    // define some variables
+    int val = 42;
+
+    // reference to the variable
+    int &refval = val;
+    /*
+     * A reference is not an object
+     * It is an alias (a different)
+     * name for the same object.*
+     */
+
+     std::cout << &refval << std::endl;
+
 
     return 0;
 }
