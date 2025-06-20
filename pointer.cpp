03541cb2e092e64bd06fcb136b1ed0cf288f4391
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-20 23:51:50 +0200

    Updated Pointer Exercise

--- pointer.cpp
+++ pointer.cpp
@@ -3,14 +3,33 @@
 
 int main() {
     // declare variable
-    int val = 42;
+    int val0 = 42;
 
     // pointer
-    int *pval = &val;
+    int *pval = &val0;
 
     // print address and value
     std::cout << "Value is: " << *pval << std::endl;    // star: dereferencing operator
     std::cout << "Address is: " << pval << std::endl;   // no star: address
+
+    // change val0 through the pointer
+    *pval = 35;
+
+    // print original value
+    std::cout << "New value of original variable is: " << val0 << std::endl;
+
+    // print the address
+    std::cout << "Address is unchanged: " << pval << std::endl;
+
+    // declare different variable
+    int val1 = 30;
+
+    // change pointer to point to new variable
+    pval = &val1;   // beware: cannot change to point to a different type variable
+
+    // print address and value
+    std::cout << "Value the pointer points to now is: " << *pval << std::endl;
+    std::cout << "Address the pointer points to now is: " << pval << std::endl; 
 
     return 0;
 }
