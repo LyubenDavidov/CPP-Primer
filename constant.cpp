dd41e412b4e67c114ef37114e2548d65c3aeba9c
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-24 23:28:53 +0200

    Aliases

--- constant.cpp
+++ constant.cpp
@@ -22,6 +22,19 @@ int main() {
     // print pi address
     std::cout << pip << std::endl; // neither the value of pi or the address can be changed
 
+    // type aliases
+    typedef double wages;
+    typedef wages base , *p;  // p is now an alias for pointer to wages that is an alias for double
+
+    wages my_wage = 5000.79;
+    p p2my_wage = &my_wage;
+
+    // print my wage and the address to it
+    std::cout << my_wage << std::endl;
+    std::cout << p2my_wage << std::endl;
+    std::cout << *p2my_wage << std::endl;
+
+
     return 0;
 }
 
