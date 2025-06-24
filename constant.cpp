afdcbd33e578d41482376411c02660a2479a1ec7
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-24 21:08:24 +0200

    Constant pointer to constant value

--- constant.cpp
+++ constant.cpp
@@ -13,6 +13,15 @@ int main() {
     // print the pointer
     std::cout << pBufSize << std::endl;
 
+    // constant pointer to constant variable
+    const double pi = 3.14159;
+
+    // constant pointer
+    const double *const pip = &pi;
+
+    // print pi address
+    std::cout << pip << std::endl;
+
     return 0;
 }
 
