95ec6171009195323609be7d88945807ea90f6b8
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-24 21:01:55 +0200

    Updated constat

--- constant.cpp
+++ constant.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 
 int main() {
-    int val = 6;
     const int BufSize = 512;
     
     // reference to const cannot be used to change the object
     const int &refBufSize = BufSize;
+
+    // pointer to constant
+    const int *pBufSize =  &BufSize;
+
+    // print the pointer
+    std::cout << pBufSize << std::endl;
 
     return 0;
 }
