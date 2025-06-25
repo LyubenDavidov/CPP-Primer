66da171a398188d0cf189a75e8727886fbd500d4
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-26 01:35:23 +0200

    Struct transactions

--- struct.cpp
+++ struct.cpp
@@ -6,10 +6,26 @@ struct Sales_data {
     double revenue = 0.0;
 };
 
-struct Foo {
-    std::string something;
-};
 
 int main() {
+    Sales_data data0, data1;
+
+    // price to calculate revenue
+    double price;
+
+    // get data from user first transaction
+    std::cin >> data0.bookNo >> data0.units_sold >> price;
+
+    // calculate the revenue based on sold units/price
+    data0.revenue = data0.units_sold * price;
+
+    // get data from user second transaction
+    std::cin >> data1.bookNo >> data1.units_sold >> price;
+
+    // calculate the revenue based on sold units/price
+    data1.revenue = data1.units_sold * price;
+
+
+    
     return 0;
 }
