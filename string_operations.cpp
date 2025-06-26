1ea24441b50a8a564f04ed03f042850f87444e1b
LyubenDavidov <ljubo.davidov@gmail.com>
2025-06-26 13:22:55 +0200

    Updated script operations

--- string_operations.cpp
+++ string_operations.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 int main() {
     string word;
-    while (cin >> word) {
-        cout << word << endl;
+
+    // this will separate the string input into individual words
+    /*
+     *   while (cin >> word) {
+     *       cout << word << endl;
+     *   }
+     */
+
+    // this will print the entire string line until newline or end-of-script
+    string line;
+    while (getline(cin, line)) {
+        cout << line << endl;
     }
     return 0;
 }
