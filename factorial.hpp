5a83390b85d3711294434985fe3741743c38be07
Lyuben Davidov <ljubo.davidov@gmail.com>
2025-07-21 00:43:25 +0300

    Completed find_char() function

--- factorial.hpp
+++ factorial.hpp
@@ -1,6 +1,8 @@
-#ifndef FACTORIAL_H
-#define FACTORIAL_H
+#ifndef UTILS_H
+#define UTILS_H
 
+#include <string>
 int factorial(int val);
+std::string::size_type find_char(const std::string &s, char c, std::string::size_type &occurs);
 
 #endif
--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "factorial.hpp"
+#include <string>
 using namespace std;
 
 
@@ -11,6 +12,11 @@ int main(){
     cout << "It also works with convertable types: " << endl;
     cout << factorial(3.14) << endl;
 
-    cout << "Fill parameter list" << endl;
+    cout << "Test the find_char() function\n" << endl;
+    cout << "This is my string sentence. Find the \'f\' characters!" << endl;
+    string s = "This is my string sentence. Find the \'f\' characters!";
+    string::size_type ocr = 0;
+    cout << "The position of \'f\' is: " << find_char(s, 'f', ocr) << endl;
+    cout << "\'f\' has been found " << ocr << " times" << endl;
     return 0;
 }
