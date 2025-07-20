1c9a41b12bd03264f6b49aeed5355488bf1564d0
Lyuben Davidov <ljubo.davidov@gmail.com>
2025-07-21 00:00:38 +0300

    Added find_char()

--- factorial.cpp
+++ factorial.cpp
@@ -1,9 +1,15 @@
 #include "factorial.hpp"
+using namespace std;
 
 int factorial(int val){
     int ret = 1;
     while (val > 1){
         ret *= val--;
     }
+    return ret;
+}
+
+atring::size_type find_char(const string &s, char c, string::size_type &occurs){
+    auto ret = s.size();
     return ret;
 }
